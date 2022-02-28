f38477ae0984224e8f21c7c6a01f5a9d302175e8
daeokar <darokar.aniket1999@gmail.com>
2022-02-28 09:33:00 +0530

    create models/update html page

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,10 @@
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+
+class student(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    
