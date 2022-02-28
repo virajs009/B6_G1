57faaf2279a838f75fec1c2843826332d9d9fad4
niraj-m <nirajmanekar10@gmail.com>
2022-02-28 14:29:33 +0530

    commit by Niraj

--- app1/models.py
+++ app1/models.py
@@ -7,4 +7,11 @@ class student(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=100)
+
+
+    class Meta:
+        db_table = "student"
+
+
+ 
     
